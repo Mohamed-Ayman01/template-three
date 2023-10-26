29126fddadd5107163a1a03f0809ac0dd76c8bb0
Mohamed-Ayman01 <lord39597@gmail.com>
2023-10-26 20:08:41 +0400

    UI: stats section

--- js/main.js
+++ js/main.js
@@ -1,18 +1,46 @@
-let skillBars = document.querySelectorAll(".our-skills .skill .progress-bar div");
+// ! change skills bars width
+let skillBars = document.querySelectorAll(
+  ".our-skills .skill .progress-bar div",
+);
 let skillsSect = document.querySelector(".our-skills");
 
-function checkIfSkillSectReached(e) {
-  console.log(1);
+function onSkillSectReached(e) {
+  if (!(skillsSect.getBoundingClientRect().top <= 300)) return;
 
-  if (skillsSect.getBoundingClientRect().top <= 300) {
-    skillBars.forEach((bar) => bar.style.width = bar.getAttribute("data-progress"));
-    
-    window.removeEventListener("scroll", checkIfSkillSectReached);
-  }
+  skillBars.forEach(
+    (bar) => (bar.style.width = bar.getAttribute("data-progress")),
+  );
+
+  window.removeEventListener("scroll", onSkillSectReached);
 }
 
-window.addEventListener("scroll", checkIfSkillSectReached)
+window.addEventListener("scroll", onSkillSectReached);
 
+// ! mega menu btn prevent default
 let megaMenuLi = document.querySelector("nav .links .mega-menu-li");
 
-megaMenuLi.onclick = e => e.preventDefault();
+megaMenuLi.onclick = (e) => e.preventDefault();
+
+// ! start stats section counters
+let statsSection = document.querySelector("#stats");
+let statsCountElements = document.querySelectorAll("#stats .box .count");
+
+function onStatsSectionReached(e) {
+  if (!(statsSection.getBoundingClientRect().top <= 100)) return;
+
+  statsCountElements.forEach((el) => {
+    let dataCount = +el.getAttribute("data-count");
+
+    let i = 1;
+
+    let handler = setInterval(() => {
+      
+      el.textContent = i++;
+      if (i > dataCount) clearInterval(handler);
+    }, 20);
+  });
+
+  window.removeEventListener("scroll", onStatsSectionReached);
+}
+
+window.addEventListener("scroll", onStatsSectionReached);
